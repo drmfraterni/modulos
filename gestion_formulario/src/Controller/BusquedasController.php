a27e941ea87955ee298f85b413de13c469ad403c
David Robledo Morante <david.robledo@alten.es>
2020-04-28 23:11:12 +0200

    Aplicación de las correcciones de la lista y de la reducción proporcional

--- gestion_formulario/src/Controller/BusquedasController.php
+++ gestion_formulario/src/Controller/BusquedasController.php
@@ -71,8 +71,8 @@ class BusquedasController extends ControllerBase {
 
     $thijos = array();
     // Máximos de sueldo por hijos/as
-    $thijos['max'][0]= 1089.09;
-    $thijos['max'][1]= 1254.86;
+    $thijos['max'][0]= 1098.09;
+    $thijos['max'][1]= 1254.96;
     $thijos['max'][2]= 1411.83;
     //Mínimos de sueldos por hijos/as
     $thijos['min'][0]= 501.98;
@@ -85,80 +85,87 @@ class BusquedasController extends ControllerBase {
 
     // TOPES SI SUPERAS EL MÁXIMO PRA LOS 6 PRIMEROS MESES
     if ($seisMeses >= $thijos['max'][0] ){ // Si el salario reduccido es mayor igual a 1089 €
-        //var_dump($hijos);
-        //var_dump($seisMeses);
         if ($hijos == "1" ){ // si se tiene un hijo es de 1254 €
           if ($seisMeses >= $thijos['max'][1] ){
-            $salarioTotalprimero = $thijos['max'][1];
+            $salarioTotalprimero = ($thijos['max'][1] * $reduccion);
             $mensajes['salarioTotalControl1 '] = $salarioTotalprimero;
           }else{
-            $salarioTotalprimero = $seisMeses;
+            $salarioTotalprimero = ($seisMeses * $reduccion);
             $mensajes['salarioTotalControl1 '] = $salarioTotalprimero;
           }
 
         }elseif ($hijos >= 2) { // si se tiene 2 hijos es de 1411 €
           if ($seisMeses >= $thijos['max'][2]){
-            $salarioTotalprimero = $thijos['max'][2];
+            $salarioTotalprimero = ($thijos['max'][2] * $reduccion);
             $mensajes['salarioTotalControl2 '] = $salarioTotalprimero;
           }else {
-            $salarioTotalprimero = $seisMeses;
+            $salarioTotalprimero = ($seisMeses * $reduccion);
             $mensajes['salarioTotalControl2 '] = $salarioTotalprimero;
           }
 
         }else{
-          $salarioTotalprimero = $thijos['max'][0];
+          $salarioTotalprimero = ($thijos['max'][0] * $reduccion);
           $mensajes['salarioTotalControl3 '] = $salarioTotalprimero;
         }
     }else{
-        $salarioTotalprimero = $seisMeses;
+        $salarioTotalprimero = ($seisMeses * $reduccion);
+        $mensajes['salarioTotalControl4 '] = $salarioTotalprimero;
+        $mensajes['comprobarSeisMeses'] = $seisMeses;
+        $mensajes['comprobarReduccion'] = $reduccion;
     }
 
     // TOPES SI SUPERAS EL MÁXIMO A PARTIR DEL SÉPTIMO MES
     if ($septimoMes >= $thijos['max'][0] ){ // Si el salario reduccido es mayor igual a 1089 €
-        //var_dump($hijos['max'][0]);
-        //var_dump($septimoMes);
         if ($hijos == "1" ){ // si se tiene un hijo es de 1254 €
           if ($septimoMes >= $thijos['max'][1] ){
-            $salarioTotalsegundo= $thijos['max'][1];
-            $mensajes['salarioTotalControl4 '] = $salarioTotalsegundo;
+            $salarioTotalsegundo= ($thijos['max'][1] * $reduccion);
+            $mensajes['salarioTotalControl5 '] = $salarioTotalsegundo;
+
           }else{
-            $salarioTotalsegundo = $septimoMes;
-            $mensajes['salarioTotalControl4 '] = $salarioTotalsegundo;
+            $salarioTotalsegundo = ($septimoMes * $reduccion);
+            $mensajes['salarioTotalControl5 '] = $salarioTotalsegundo;
           }
 
         }elseif ($hijos >= 2) { // si se tiene 2 hijos es de 1411 €
           if ($septimoMes >= $thijos['max'][2]){
-            $salarioTotalsegundo = $thijos['max'][2];
-            $mensajes['salarioTotalControl5 '] = $salarioTotalprimero;
+            $salarioTotalsegundo = ($thijos['max'][2] * $reduccion);
+            $mensajes['salarioTotalControl6 '] = $salarioTotalsegundo;
           }else {
             $salarioTotalsegundo = $septimoMes;
-            $mensajes['salarioTotalControl5 '] = $salarioTotalprimero;
+            $mensajes['salarioTotalControl6 '] = $salarioTotalsegundo;
           }
 
         }else{ // máximo  básico 1084 €
-          $salarioTotalsegundo = $thijos['max'][0];
-          $mensajes['salarioTotalControl6 '] = $salarioTotalsegundo;
+          $salarioTotalsegundo = ($thijos['max'][0] * $reduccion);
+          $mensajes['salarioTotalControl7 '] = $salarioTotalsegundo;
         }
     } else {
-          $salarioTotalsegundo = $septimoMes;
+          $salarioTotalsegundo = ($septimoMes * $reduccion);
+          $mensajes['salarioTotalControl8 '] = $salarioTotalsegundo;
 
     }
 
     // TOPES SI NOS LLEGAS AL MÍMINO EN LOS SEIS PRIMEROS MESES
-    if ($seisMeses <= $thijos['min'][0]){
+    if ($seisMeses <= $thijos['min'][1]){
       if ($hijos == 0){
-        $salarioTotalprimero = $thijos['min'][0];
+        $salarioTotalprimero = ($thijos['min'][0] * $reduccion);
+        $mensajes['salarioTotalControl9 '] = $salarioTotalprimero;
       }else{
-        $salarioTotalprimero = $thijos['min'][1];
+        $salarioTotalprimero = ($thijos['min'][1] * $reduccion);
+        $mensajes['salarioTotalControl9 '] = $salarioTotalprimero;
       }
 
     }
     // TOPES SI NOS LLEGAS AL MÍMINO A PARIR DEL SÉPTIMO MES
-    if ($septimoMes <= $thijos['min'][0]){
+
+    if ($septimoMes <= $thijos['min'][1]){
+
       if ($hijos == 0){
-        $salarioTotalsegundo = $thijos['min'][0];
+        $salarioTotalsegundo = ($thijos['min'][0] * $reduccion);
+        $mensajes['salarioTotalControl10'] = $salarioTotalsegundo;
       }else{
-        $salarioTotalsegundo = $thijos['min'][1];
+        $salarioTotalsegundo = ($thijos['min'][1] * $reduccion);
+        $mensajes['salarioTotalControl10 '] = $salarioTotalsegundo;
       }
 
     }
@@ -168,7 +175,7 @@ class BusquedasController extends ControllerBase {
     $mensajes['salarioTotal'] = self::redondear($salarioTotal);
     $mensajes['jornada'] = self::redondear($reduccion * 8);
 
-    var_dump($mensajes);
+    // var_dump($mensajes);
 
     // PARAMETROS QUE PASAMOS A LA VISTA PARA PINTAR TODOS LOS RESULTADOS
 
