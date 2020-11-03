a8510ea100d665b46f9509fb72204817a4816d07
David Robledo Morante <david.robledo@alten.es>
2020-11-03 10:57:15 +0100

    Actualización del módulo y del tema Copia de seguridad 20201103 b

--- elcej/js/global.js
+++ elcej/js/global.js
@@ -3,3 +3,20 @@
  * Global utilities.
  *
  */
+
+    var verImag = document.querySelector('#mas-img');
+    
+    verImag.onclick = function (){
+    	var nuevoDiv = document.createElement('div');
+	    nuevoDiv.classList.add('nuevo-mensaje');
+	    var nuevoEstilo = document.querySelector('.nuevo-mensaje')
+	    var contenidoDiv = document.createTextNode("esto es una prueba");
+	    nuevoDiv.appendChild(contenidoDiv);
+
+	    verImag.appendChild(nuevoDiv);
+    	alert('Esto es una prueba');
+    }
+
+
+    console.log(verImag);
+
